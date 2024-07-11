e4d916c754b075a319ef196f6822f3dc68192683
lefpap <lolopap21@gmail.com>
2024-07-11 20:54:27 +0300

    bugfix: loaded theme not applying corectly
    The fix was to change the base.css file and add the :root[class=~="dark"] instead of plain :root

--- astro-ssg/public/js/theme-loader.js
+++ astro-ssg/public/js/theme-loader.js
@@ -1,32 +1,48 @@
-function loadTheme(document) {
-  const isBrowser = typeof localStorage !== "undefined";
+const getThemePreference = () => {
+  if (localStorage.getItem("theme")) {
+    return localStorage.getItem("theme");
+  }
+  return window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light";
+};
 
-  const getThemePreference = () => {
-    if (isBrowser && localStorage.getItem("theme")) {
-      return localStorage.getItem("theme");
-    }
-    return window.matchMedia("(prefers-color-scheme: dark)").matches
-      ? "dark"
-      : "light";
-  };
+const applyTheme = (document, theme) => {
+  const isDark = theme === "dark";
+  document.documentElement.classList[isDark ? "add" : "remove"]("dark");
+};
 
-  if (isBrowser) {
-    const observer = new MutationObserver(() => {
-      const isDark = document.documentElement.classList.contains("dark");
-      localStorage.setItem("theme", isDark ? "dark" : "light");
-    });
-    observer.observe(document.documentElement, {
-      attributes: true,
-      attributeFilter: ["class"],
-    });
+const setupObserver = (document) => {
+  console.log("Setting up observer");
+
+  const observer = new MutationObserver(() => {
+    const isDark = document.documentElement.classList.contains("dark");
+    localStorage.setItem("theme", isDark ? "dark" : "light");
+  });
+
+  observer.observe(document.documentElement, {
+    attributes: true,
+    attributeFilter: ["class"],
+  });
+};
+
+const loadTheme = (document) => {
+  const IS_SERVER = typeof localStorage === "undefined";
+  if (IS_SERVER) {
+    console.warn(
+      "localStorage is not available on the server, skipping theme setup.",
+    );
   }
 
-  const isDark = getThemePreference() === "dark";
-  document.documentElement.classList[isDark ? "add" : "remove"]("dark");
-}
+  const theme = getThemePreference();
+  setupObserver(document);
+  applyTheme(document, theme);
+};
 
+// Load the theme on initial page load
 loadTheme(document);
 
+// Listen for the `astro:before-swap` event and then set the theme on the incoming document
 document.addEventListener("astro:before-swap", (ev) => {
   // Pass the incoming document to set the theme on it
   loadTheme(ev.newDocument);
